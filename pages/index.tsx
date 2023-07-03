29a2ab92382b9c4e5c8edd568e25947f15790e06
clubdrop <94155502+hashbomb@users.noreply.github.com>
2023-07-03 19:33:00 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -212,8 +212,18 @@ const Home: NextPage = () => {
         {isLoading ? (
           <p>Loading...</p>
         ) : (
-          
+          <>
+            <div className={styles.infoSide}>
+              {/* The removed sections */}
+            </div>
 
+            <div className={styles.imageSide}>
+              {/* Image Preview of NFTs */}
+              <img
+                className={styles.image}
+                src={contractMetadata?.image}
+                alt={`${contractMetadata?.name} preview image`}
+              />
 
               {/* Amount claimed so far */}
               <div className={styles.mintCompletionArea}>
@@ -234,64 +244,7 @@ const Home: NextPage = () => {
                 </div>
               </div>
 
-              {claimConditions.data?.length === 0 ||
-              claimConditions.data?.every(
-                (cc) => cc.maxClaimableSupply === "0"
-              ) ? (
-                <div>
-                  <h2>
-                    This drop is not ready to be minted yet. (No claim condition
-                    set)
-                  </h2>
-                </div>
-              ) : (
-                <>
-                  <p>Quantity</p>
-                  <div className={styles.quantityContainer}>
-                    <button
-                      className={`${styles.quantityControlButton}`}
-                      onClick={() => setQuantity(quantity - 1)}
-                      disabled={quantity <= 1}
-                    >
-                      -
-                    </button>
-
-                    <h4>{quantity}</h4>
-
-                    <button
-                      className={`${styles.quantityControlButton}`}
-                      onClick={() => setQuantity(quantity + 1)}
-                      disabled={quantity >= maxClaimable}
-                    >
-                      +
-                    </button>
-                  </div>
-
-                  <div className={styles.mintContainer}>
-                    {isSoldOut ? (
-                      <div>
-                        <h2>Sold Out</h2>
-                      </div>
-                    ) : (
-                      <Web3Button
-                        contractAddress={editionDrop?.getAddress() || ""}
-                        action={(cntr) => cntr.erc1155.claim(tokenId, quantity)}
-                        isDisabled={!canClaim || buttonLoading}
-                        onError={(err) => {
-                          console.error(err);
-                          alert("Error claiming NFTs");
-                        }}
-                        onSuccess={() => {
-                          setQuantity(1);
-                          alert("Successfully claimed NFTs");
-                        }}
-                      >
-                        {buttonLoading ? "Loading..." : buttonText}
-                      </Web3Button>
-                    )}
-                  </div>
-                </>
-              )}
+              {/* The removed sections */}
             </div>
           </>
         )}
